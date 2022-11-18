26f447fa9285e511023f34a4112121f86e888713
sakshikhule <102469697+sakshikhule@users.noreply.github.com>
2022-11-18 18:01:14 +0530

    Add files via upload

---
+++ day 3.cpp
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<conio.h>
+using namespace std;
+main()
+{
+   char n;
+   printf("enter any char = ");
+   scanf("%c",&n);
+   printf("Ascii value of char = %d",n);
+   getch();
+}
